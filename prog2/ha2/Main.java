c69fef6a6097ecb59270e61b97025dec17024c18
hyperxpizza <wojtek.frackowski@gmail.com>
2020-10-20 23:46:53 +0200

    add constructor

---
+++ prog2/ha2/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String args[]){
+        Person p1 = new Person("Muster", "Max", false, 1, true, 450);
+        System.out.println(p1);
+    }
+}
--- prog2/ha2/Person.java
+++ prog2/ha2/Person.java
@@ -3,15 +3,23 @@
 public class Person{
     private String name;
     private String vorname;
-    private boolean fuehrungsrolleUbernehman;
+    private boolean fuehrungsrolleUbernehmen;
+    private int betrieb;
     private boolean azubi;
     private int gehalt;
 
 
-    public Person(String name, String vorname, boolean fuehrungsrolleUbernehman, int gehalt){
+    public Person(String name, String vorname, boolean fuehrungsrolleUbernehmen, int betrieb, boolean azubi, int gehalt){
         this.name = name;
         this.vorname = vorname;
+        this.fuehrungsrolleUbernehmen = fuehrungsrolleUbernehmen;
+        this.gehalt = gehalt;
+        this.betrieb = betrieb;
+        this.azubi = azubi;
     }
 
-
+    public String toString(){
+        String message = this.vorname + " " + this.name;
+        return message;
+    }
 }
