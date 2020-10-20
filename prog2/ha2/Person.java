7585d482332d960ac2a4c589eb94b7f11a3dd5f6
hyperxpizza <wojtek.frackowski@gmail.com>
2020-10-20 23:26:34 +0200

    cleanup

---
+++ prog2/ha2/Person.java
@@ -0,0 +1,17 @@
+
+
+public class Person{
+    private String name;
+    private String vorname;
+    private boolean fuehrungsrolleUbernehman;
+    private boolean azubi;
+    private int gehalt;
+
+
+    public Person(String name, String vorname, boolean fuehrungsrolleUbernehman, int gehalt){
+        this.name = name;
+        this.vorname = vorname;
+    }
+
+
+}
