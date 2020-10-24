124c00115a189816fccc383669ce4d37114954f5
hyperxpizza <wojtek.frackowski@gmail.com>
2020-10-24 20:31:38 +0200

    update

--- prog2/ha3/Administration.java
+++ prog2/ha3/Administration.java
@@ -1,3 +1,28 @@
 public class Administration {
-    
+ 
+    public static int countFemale(Person[] list){
+        int count;
+        for(int i=0; i<list.length; i++){
+            if(list[i].geschlecht = 'f'){
+                count++
+            }
+        }
+
+        return count;
+    }
+
+    public Person[] createList(Person... persons){
+        
+    }
+
+    public static java.lang.String outputList(Person[] list) throws java.lang.NullPointerException {
+
+    }
+
+    public static Person[] sortList(Person[] list) throws java.lang.NullPointerException {
+
+    }
+
+
+
 }
