955f2dddcbe904b93e9fedb39b6b86729e7ae373
hyperxpizza <wojtek.frackowski@gmail.com>
2020-11-12 22:46:18 +0100

    add input validation

--- algo-dat/ha1/app/src/main/java/MyTestProg/Console.java
+++ algo-dat/ha1/app/src/main/java/MyTestProg/Console.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 public class Console {
     
     public static int readIntegerFromStdIn(String text){
+        //init scanner object
         Scanner sc = new Scanner(System.in);
       
-        System.out.print(text);
-        int num = sc.nextInt();
-        
-        return num;
+        int x;
+        // repeat until user gives a correct input
+        while(true){
+            //if input is an integer, break the loop, otherwise throw an exception
+            try {
+                System.out.print(text);
+                x = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (NumberFormatException nfe) {
+                System.out.println("not a valid input, try again");
+            }
+        }
+
+        return x;
     }
 
     public static String readStringFromStdIn(String text){
