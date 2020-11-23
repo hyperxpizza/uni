6f5d72c063042ee952ddf83d07caa0fbd3a1c3be
hyperxpizza <wojtek.frackowski@gmail.com>
2020-11-23 20:10:11 +0100

    start list

---
+++ prog2/ha6/Liste.java
@@ -0,0 +1,16 @@
+
+public class Liste {
+    private Node first = null;
+    private Node last = null;
+
+    public int size() {
+        Node n = this.first;
+        int length = 0;
+        while(n != null){
+            length++;
+            n = n.getNext();
+        }
+
+        return length;
+    }
+}
