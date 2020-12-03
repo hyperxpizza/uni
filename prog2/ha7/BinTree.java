6e5dcedccf81f97d93ae0192a6e3f94fca1024f6
hyperxpizza <wojtek.frackowski@gmail.com>
2020-12-03 19:26:02 +0100

    tests

--- prog2/ha7/BinTree.java
+++ prog2/ha7/BinTree.java
@@ -1,8 +1,7 @@
-
-
 public class BinTree {
-	public BinNode root = null;
-    public int prev = Integer.MIN_VALUE;
+
+    private BinNode root = null;
+    private int prev = Integer.MIN_VALUE;
 
     public BinTree(){
         root = null;
@@ -12,77 +11,108 @@ public class BinTree {
         root = rn;
     }
 
-    public int countNodes(BinNode k){
-        int counter = 0;
-        if(k != null){
-            counter = 1 + countNodes(k.left) + countNodes(k.right);
-        }
 
-        return counter;
+    public BinNode getRoot() {
+        return root;
     }
 
+   
+    /**
+     * counts the number of nodes of the binary tree having two children nodes
+     * @param k given node
+     * @return the number of nodes
+     */
     public int countTwoChildrenNodes(BinNode k){
         if(k == null){
             return 0;
         }
-
         int count = 0;
         if(k.left != null && k.right != null){
             count++;
         }
-
         count += (countTwoChildrenNodes(k.left) + countTwoChildrenNodes(k.right));
         return count;
     }
 
+    /**
+     * wrapper method for countTwoChildrenNodes(BinNode k)
+     * @return the number of nodes
+     */
+    public int countTwoChildrenNodes(){
+        return countTwoChildrenNodes(root);
+    }
+
+    /**
+     * checks either the binary tree is sorted or not
+     * @param k given node
+     * @return true if the tree is sorted
+     */
     public boolean isSorted(BinNode k){
         if(k != null){
             if(!isSorted(k.left)){
                 return false;
             }
-
             if (k.data <= prev){
                 return false;
             }
-
             prev = k.data;
-
             return isSorted(k.right);
         }
-
         return true;
     }
 
-   
-    private void insertNode(BinNode neu, BinNode k){
-        if(k != null){
-            if(neu.data < k.data){
-                if(k.left == null){
-                    k.left = neu;
-                } else {
-                    insertNode(neu, k.left);
-                }
-            }
-            if (neu.data > k.data){
-                if(k.right == null){
-                    k.right = neu;
-                }
-                else{
-                    insertNode(neu, k.right);
-                }
-            }
-            if(neu.data == k.data){
-                System.out.println("Knoten vorhanden");
-            }
-        } else {
-            root = neu;
+
+    /**
+     * wrapper method for isSorted(BinNode k)
+     * @return true if the tree is sorted
+     */
+    public boolean isSorted() throws NullPointerException{
+        if(root == null) {
+        	throw new NullPointerException();
         }
+    	return isSorted(root);
     }
 
-    public void insertNode(int data){
-        BinNode n = new BinNode(data);
-        insertNode(n, root);
+    /**
+     * return the minimum value from the tree
+     * @param k given node
+     * @return the minimum value
+     */
+    public int minValue(BinNode k){
+        if (k.left != null){
+            return minValue(k.left);
+        }
+        return k.data;
+    }
+
+    
+    
+    public boolean removeNode(int value) {
+        return (removeNode(getRoot(), value) != null);
     }
     
-   
+    public BinNode removeNode(BinNode k, int value) throws NullPointerException{
+        if(k == null){
+            throw new NullPointerException("given value is not in the tree");
+        }
+        if(value < k.data){ // search in left part of the tree if the given value is smaller than the actual value
+            k.left = removeNode(k.left, value);
+        }else if(value > k.data){ // search in right part of the tree if the given value is larger than the actual value
+            k.right = removeNode(k.right, value);
+        }else{ // node to be removed is found
+            if (k.left == null && k.right == null){
+                return null;
+            }else if(k.right == null){
+                return k.left; // replaces the node with its left child
+            }else if(k.left == null){
+                return k.right;  // replaces the node with its right child
+            }else{
+                k.data = minValue(k.right); // finds the smallest node and overwrites the value
+                k.right = removeNode(k.right, k.data); // delete the node form the right subtree
+            }
+        }
+        return k;
+    }
+  
+
 }
--- prog2/ha7/Tests.java
+++ prog2/ha7/Tests.java
@@ -4,65 +4,161 @@ import org.junit.Test;
 
 public class Tests {
 
-	@Test
-	public void testCountNodesEmpty() {
-		BinTree tree = new BinTree();
-		assertEquals(0, tree.countNodes(tree.root));
-	}
+    @Test
+    public void testCountTwoChildrenNodesEmpty() {
+        BinTree tree = new BinTree();
+        assertEquals(0, tree.countTwoChildrenNodes());
+    }
 
-	@Test
-	public void testCountNodesRoot() {
-		BinTree tree = new BinTree();
-		tree.insertNode(1);
-		assertEquals(1, tree.countNodes(tree.root));
-	}
-	
-	@Test
-	public void testCountNodes1() {
-		BinTree tree = new BinTree();
-		tree.insertNode(7);
-		tree.insertNode(9);
-		tree.insertNode(4);
-		tree.insertNode(2);
-		tree.insertNode(8);
-		assertEquals(5, tree.countNodes(tree.root));
-	}
+    @Test
+    public void testCountTwoChildrenNodesRoot() {
+        BinNode root = new BinNode(1);
+        BinTree tree = new BinTree(root);
+        assertEquals(0, tree.countTwoChildrenNodes());
+    }
 
-	@Test
-	public void testCountTwoChildrenNodesEmpty() {
-		BinTree tree = new BinTree();
-		assertEquals(0, tree.countTwoChildrenNodes(tree.root));
-	}
-	
-	@Test
-	public void testCountTwoChildrenNodesRoot() {
-		BinNode root = new BinNode(1);
-		BinTree tree = new BinTree(root);
-		assertEquals(0, tree.countTwoChildrenNodes(tree.root));
-	}
-	
-	@Test
-	public void testCountTwoChildrenNodes1() {
-		BinTree tree = new BinTree();
-		tree.insertNode(7);
-		tree.insertNode(4);
-		tree.insertNode(9);
-		tree.insertNode(2);
-		tree.insertNode(5);
-		tree.insertNode(8);
-		tree.insertNode(1);
-		tree.insertNode(3);
-		assertEquals(3, tree.countTwoChildrenNodes(tree.root));
-	}
-	
-	@Test
-	public void testCountTwoChildrenNodes2() {
-		BinTree tree = new BinTree();
-		tree.insertNode(6);
-		tree.insertNode(4);
-		tree.insertNode(9);
-		tree.insertNode(2);
-		tree.insertNode(7);
-		assertEquals(2, tree.countTwoChildrenNodes(tree.root));
-	}
+    @Test
+    public void testCountTwoChildrenNodes1() {
+    	BinNode root = new BinNode(7);
+    	BinTree tree = new BinTree(root);
+    	
+    	// 1st level
+    	root.right = new BinNode(9);
+    	root.left = new BinNode(4);
+    	
+    	//2nd level
+    	root.right.left = new BinNode(8);
+    	root.left.right = new BinNode(5);
+    	root.left.left = new BinNode(2);
+    	
+    	//3nd level
+    	root.left.left.right = new BinNode(3);
+    	root.left.left.left = new BinNode(1);
+        
+        assertEquals(3, tree.countTwoChildrenNodes());
+    }
+    
+    @Test
+    public void testCountTwoChildrenNodes2() {
+    	BinNode root = new BinNode(6);
+    	BinTree tree = new BinTree(root);
+    	// 1st level
+    	root.right = new BinNode(9);
+    	root.left = new BinNode(4);
+    	
+    	// 2nd level
+    	root.left.left = new BinNode(2);
+    	root.left.right = new BinNode(7);
+    
+    	assertEquals(2, tree.countTwoChildrenNodes());
+    }
+
+    
+    
+    
+    
+    @Test(expected = NullPointerException.class)
+    public void isSortedEmpty() {
+    	BinTree tree = new BinTree();
+    	System.out.println(tree.isSorted());
+    }
+    
+    @Test
+    public void isSortedRoot() {
+    	BinNode root = new BinNode(1);
+    	BinTree tree = new BinTree(root);
+        assertEquals(true, tree.isSorted());
+    }
+    
+    
+    @Test
+    public void testIsSorted1() {
+    	BinNode root = new BinNode(7);
+    	BinTree tree = new BinTree(root);
+    	
+    	// 1st level
+    	root.right = new BinNode(9);
+    	root.left = new BinNode(4);
+    	
+    	//2nd level
+    	root.right.left = new BinNode(8);
+    	root.left.right = new BinNode(5);
+    	root.left.left = new BinNode(2);
+    	
+    	//3nd level
+    	root.left.left.right = new BinNode(3);
+    	root.left.left.left = new BinNode(1);
+    	
+        
+        assertEquals(true, tree.isSorted());
+    }
+    
+    @Test
+    public void testIsSorted2() {
+    	BinNode root = new BinNode(6);
+    	BinTree tree = new BinTree(root);
+    	// 1st level
+    	root.right = new BinNode(9);
+    	root.left = new BinNode(4);
+    	
+    	// 2nd level
+    	root.left.left = new BinNode(2);
+    	root.left.right = new BinNode(7);
+        
+        assertEquals(false, tree.isSorted());
+        
+    }
+   
+   
+    @Test(expected = NullPointerException.class)
+    public void testRemoveNodeEmpty() {
+    	BinTree tree = new BinTree();
+    	assertEquals(false, tree.removeNode(1));
+    }
+    
+    @Test
+    public void testRemoveNodeRoot() {
+    	BinNode root = new BinNode(6);
+    	BinTree tree = new BinTree(root);
+    	System.out.println(tree.removeNode(6));
+    	
+    }
+    
+    @Test
+    public void testRemoveNode1() {
+    	BinNode root = new BinNode(7);
+    	BinTree tree = new BinTree(root);
+    	
+    	// 1st level
+    	root.right = new BinNode(9);
+    	root.left = new BinNode(4);
+    	
+    	//2nd level
+    	root.right.left = new BinNode(8);
+    	root.left.right = new BinNode(5);
+    	root.left.left = new BinNode(2);
+    	
+    	//3nd level
+    	root.left.left.right = new BinNode(3);
+    	root.left.left.left = new BinNode(1);
+    	assertEquals(true, tree.removeNode(1));
+    }
+    
+    @Test
+    public void testRemoveNode2() {
+    	BinNode root = new BinNode(6);
+    	BinTree tree = new BinTree(root);
+    	// 1st level
+    	root.right = new BinNode(9);
+    	root.left = new BinNode(4);
+    	
+    	// 2nd level
+    	root.left.left = new BinNode(2);
+    	root.left.right = new BinNode(7);
+    	assertEquals(true, tree.removeNode(4));
+    }
+   
+    
+   
+    
 }
